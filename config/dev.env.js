166aca3c6a3c869088f672a4c38952376faad985
heyongheyong <heyongheyong@didiglobal.com>
2025-01-06 20:31:20 +0800

    init

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  BASE_API: '"http://8.140.193.241:8080"'
 })
